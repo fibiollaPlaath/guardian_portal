317e1f2251551acb2cd83c84b484300a40e5aca1
Fibiolla Plaath <Fibiolla.Plaath@green.com.na>
2023-05-23 14:47:31 +0200

    login logout logic

--- app/src/main/java/com/example/guardian_portal/screens/authentication/LoginFragment.kt
+++ app/src/main/java/com/example/guardian_portal/screens/authentication/LoginFragment.kt
@@ -5,12 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
+import com.example.guardian_portal.R
 import com.example.guardian_portal.databinding.FragmentLoginBinding
-import com.example.guardian_portal.databinding.FragmentWelcomeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginFragment : Fragment() {
 
     private var binding: FragmentLoginBinding? = null
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initializing Firebase Auth
+        auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +33,45 @@ class LoginFragment : Fragment() {
         return binding!!.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+
+            buttonContinue.setOnClickListener {
+                loginUser()
+            }
+
+        }
+
+    }
+
+    private fun loginUser() {
+
+        val email = binding?.editTextEmail?.text.toString()
+        val password = binding?.editTextPassword?.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Login successful -> Navigate to next screen
+                    findNavController().navigate(R.id.action_LoginFragment_to_homeFragment)
+                } else {
+                    // Login failed
+                    Toast.makeText(requireContext(), "Login failed. Please try again.", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
+    private fun clearUI() {
+        binding?.editTextEmail?.text?.clear()
+        binding?.editTextPassword?.text?.clear()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        clearUI()
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/guardian_portal/screens/home/HomeFragment.kt
+++ app/src/main/java/com/example/guardian_portal/screens/home/HomeFragment.kt
@@ -5,16 +5,59 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.guardian_portal.R
+import com.example.guardian_portal.databinding.FragmentHomeBinding
+import com.example.guardian_portal.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class HomeFragment : Fragment() {
+
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initializing Firebase Auth
+        auth = Firebase.auth
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding?.apply {
+
+            textViewLogout.setOnClickListener {
+                logOutUser()
+            }
+
+        }
+
+    }
+
+    private fun logOutUser() {
+
+        // Log out and navigate to login screen
+        auth.signOut()
+        findNavController().popBackStack()
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
     }
 
 }
